2b76f091b34bfd45daa5ec72612e0b06e80f94ee
SoftDynasty <softdynasty2019@gmail.com>
2020-07-01 20:45:13 -0400

    new code

---
+++ NewUpdate/src/EbayHomeDemmo.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.Test;
+
+public class EbayHomeDemmo {
+
+	@Test
+	public void usa() {
+		
+		
+		System.out.println("This is usa Code");
+		
+		System.out.println("Pushing new code");
+	}
+	
+	@Test
+public void bd() {
+		
+	System.out.println("code from BD");
+	
+	
+	}
+	
+	@Test
+public void bd2() {
+		
+	System.out.println("code from BD");
+	
+	
+	}
+	
+	
+	
+}
+
+	
+	
+	
+}
